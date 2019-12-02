bf5a7ba4b4a66238b384c633ceaaf43ebc52136c
Miguel Oliveira <miguelmigpt@gmail.com>
2019-12-02 15:32:44 +0000

    Posting post code implemented

--- app/routes/api.js
+++ app/routes/api.js
@@ -4,6 +4,13 @@ var posts = require('../controllers/posts');
 var users = require('../controllers/users');
 var url = require('url');
 
+const multer = require('multer');
+var upload = multer({dest: 'uploads/'});
+
+const Ficheiro = require('../models/file');
+
+
+
 // User Api
 router.get('/user/:userid', (req, res) => {
     var userid = req.params.userid;
@@ -29,13 +36,53 @@ router.get('/user/:userid/posts', (req, res) => {
     .catch(err => res.status(500).jsonp(err));
 })
 
-router.post('/post', (req, res) => {
-    posts.add(req.body)
-        .then(data => res.jsonp(data))
-        .catch(err => res.status(500).jsonp(err));
+router.post('/post', upload.array('ficheiro'), (req, res) => {
+
+    var promises = [];
+
+    let date = new Date();
+    let post = {
+        date: date.toISOString(),
+        user_id: req.user.id,
+        title: req.body.title,
+        tags: req.body.tags,
+        content: req.body.content,
+        attachments: [],
+        comments: [],
+    };
+
+    for(let i = 0; i < req.files.length; ++i)
+    {
+        let current_file = req.files[i];
+        let old_path = __dirname + "/../" + current_file.path;
+        let new_path = __dirname + '/../public/ficheiros' + current_file.originalname;
+        
+        fs.rename(old_path, new_path, err => {
+            if(err)
+            {
+                throw err;
+            }
+        });
+        
+        post.attachments.push(new Ficheiro({
+            name: current_file.originalname,
+            mimetype: current_file.mimetype,
+            size: current_file.size
+        }));
+    }
+
+    post.save()
+    .then(post => {
+        console.log("Post saved with sucess!");
+        res.jsonp(post);
+    })
+    .catch(err => {
+        console.error("Error saving a post");
+        res.status(500).jsonp(err);
+    });
 })
 
-router.post('/post/:postid', (req, res) => {
+router.get('/post/:postid', (req, res) => {
     var postid = req.params.postid;
     posts.getById(postid)
     .then(data => res.jsonp(data))
