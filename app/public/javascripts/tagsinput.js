e4fbc6dd3b0345b88745c58bc7c1ac1d710e3bb6
Pedro Moura <pedrorpmoura@gmail.com>
2020-01-22 23:14:55 +0000

    Tags a funcionar!

--- app/public/javascripts/tagsinput.js
+++
@@ -1,7 +0,0 @@
-const tagify = require('../../node_modules/@yaireo/tagify/src/tagify');
-
-// The DOM element you wish to replace with Tagify
-var input = document.querySelector('input[name=tags]');
-
-// init Tagify script on the above inputs
-tagify.Tagify(input)
