0eda9e7b0648b9699fa0562c779aec09d8690d83
Pedro Moura <pedrorpmoura@gmail.com>
2020-01-22 17:30:21 +0000

    update

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     id: { type: String, required: true },
-    username: { type: String, required: true },
     password: { type: String, required: true },
     full_name: { type: String, required: true },
     course: { type: String, required: true },
