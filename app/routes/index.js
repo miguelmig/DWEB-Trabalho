1b3939cc8ea8d7406f847914ef6fd7e35cc0dc9c
Miguel Oliveira <miguelmigpt@gmail.com>
2020-01-22 22:32:20 +0000

    fix auth, make remember me work

--- app/routes/index.js
+++ app/routes/index.js
@@ -57,13 +57,14 @@ router.get('/login', function(req, res) {
 
 router.post('/login', passport.authenticate('local', {
 		failureRedirect: "/login"
-	}), function (req, res) {
-		if (req.body["remember-me"]) {
-			req.session.cookie.maxAge = 30 * 24 * 60 * 60 * 1000; // Cookie expires after 30 days
-		  } else {
-			req.session.cookie.expires = false; // Cookie expires at end of session
-		  }
-		res.redirect('/');
+	}), function(req,res) {
+			if (req.body.remember) {
+				req.session.cookie.maxAge = 30 * 24 * 60 * 60 * 1000; // Cookie expires after 30 days
+			} 
+			else {
+				req.session.cookie.expires = false; // Cookie expires at end of session
+			}
+			res.redirect('/');
 	}
 );
 
