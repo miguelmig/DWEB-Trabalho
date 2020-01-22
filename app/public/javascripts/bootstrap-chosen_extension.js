c0c418d32185f324c3c4324287ae79bf7151b95b
Miguel Oliveira <miguelmigpt@gmail.com>
2020-01-22 18:32:25 +0000

    Add changes

---
+++ app/public/javascripts/bootstrap-chosen_extension.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('.chosen-select').chosen();
+    $('.chosen-search-input').css({'padding-left': '10px'});
+    //$('.chosen-choices').css({"height": "70px"});
+    //$('.chosen-')
+    //$('.chosen-select-deselect').chosen({ allow_single_deselect: true, placeholder_text_multiple: "Teste" });
+});
