37d39436cfac91529eccc9712790cf63889c930a
Manuel0Borba <47677390+Manuel0Borba@users.noreply.github.com>
2020-01-24 17:30:53 +0000

    db mongo e view pug

---
+++ chat/lib/socketio.js
@@ -0,0 +1,50 @@
+/* 
+ *
+ */
+
+
+var Messages = require('../controllers/chat_message')
+var socket_io = require('socket.io');
+var io       = socket_io();
+var socketio = {};
+socketio.io  = io;
+var users = [];
+ io.on('connection', function(socket){
+    
+    console.log('A user connected');
+console.log(`Socket connected ${socket.id}`)
+
+    Messages.getpreviousMsgs()
+        .then(dados =>socket.emit('previousMessage', dados))
+        .catch(erro =>res.status(500).jsonp(erro)) 
+        
+        socket.on('sendMessage', data => {
+            console.log(data);
+            Messages.create(data)
+                .then(dados =>console.log("Saved"))
+                .catch(erro =>res.status(500).jsonp(erro))
+        });
+    socket.on('join', function (user){
+       socket.username = user.username;
+       users.push(socket.username);
+       io.emit('user joined', { 'username': user.username, users:users });
+    });
+
+    
+    socket.on('typing', function (msg) {
+        io.emit('typing', { 'message': msg.message, 'username': msg.username });
+    });
+
+    socket.on('new_message', function (msg) {
+         io.emit('chat message', { 'message': msg.message, 'username': msg.username });
+    });
+    
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+        users.splice(users.indexOf(socket.username), 1);
+        io.emit('user disconnected', { 'username': socket.username });
+    });
+     
+ });
+ 
+module.exports = socketio;
