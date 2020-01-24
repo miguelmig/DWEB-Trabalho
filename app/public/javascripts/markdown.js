e60999cb0c55696036ada4c6192106f85bca873c
Miguel Oliveira <miguelmigpt@gmail.com>
2020-01-24 12:45:56 +0000

    remove margin on comments

--- app/public/javascripts/markdown.js
+++ app/public/javascripts/markdown.js
@@ -6,5 +6,8 @@ $(function ()
         var markdown_text = element.text();
         var unsanitized_html = marked(markdown_text);
         $(this).html(unsanitized_html);
+        $(this).children("p").each(function(index) {
+            this.classList.add("mb-0")
+        })
     })
 });
