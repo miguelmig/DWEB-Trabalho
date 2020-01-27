90221ab4fc3d499e55a3611428d2ccf13d95e580
Pedro Moura <pedrorpmoura@gmail.com>
2020-01-27 00:03:54 +0000

    Update posts.js

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -263,8 +263,11 @@ module.exports.getBySearch = (search) => {
                     "as": "poster"
                 }
             },
-            {
-                "$unwind": "$comments",
+            { 
+                "$unwind": { 
+                "path":"$comments",
+                "preserveNullAndEmptyArrays": true
+                }
             },
             {
                 "$lookup": {
@@ -289,15 +292,15 @@ module.exports.getBySearch = (search) => {
                     "poster": {"$first": "$poster"}
                 }
             },
-            {
-                "$match": {
-                    "$or": [
-                        { "title": { "$regex": search, "$options": "$i" } },
-                        { "content": { "$regex": search, "$options": "$i" } },
-                        { "comments.comment.content": { "$regex": search, "$options": "$i" } }
-                    ]
-                }
-            }
+            //{
+            //    "$match": {
+            //        "$or": [
+            //            { "title": { "$regex": search, "$options": "$i" } },
+            //            { "content": { "$regex": search, "$options": "$i" } },
+            //            { "comments.comment.content": { "$regex": search, //"$options": "$i" } }
+            //        ]
+            //    }
+            //}
         ])
         .exec();
 
