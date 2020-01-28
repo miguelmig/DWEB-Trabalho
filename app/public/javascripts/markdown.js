e4e91557f73e083f882bcaa5fe0ea3f98a51d12b
Miguel Oliveira <miguelmigpt@gmail.com>
2020-01-28 11:41:46 +0000

    links open in a new tab in markdown

--- app/public/javascripts/markdown.js
+++ app/public/javascripts/markdown.js
@@ -2,10 +2,16 @@ $(function ()
 {
     $('.markdown-content').each(function(index) {
         var element = $(this);
-        console.log(element);
+        //console.log(element);
         var markdown_text = element.text();
         var unsanitized_html = marked(markdown_text);
         $(this).html(unsanitized_html);
+        
+        $(this).find("a").each(function(index){
+            console.log(this);
+            $(this).attr('target', "_blank");
+        })
+        
         $(this).children("p").each(function(index) {
             this.classList.add("mb-0")
         })
